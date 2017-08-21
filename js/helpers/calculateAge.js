548763a99e691fdbc1e4f2393fae4f8209d6080c
T. M. Jones <timothy.jones.phd@gmail.com>
2017-08-21 15:31:18 -0500

    Add files via upload

--- js/helpers/calculateAge.js
+++ js/helpers/calculateAge.js
@@ -2,7 +2,7 @@ module.exports = function(birthYear) {
     var age = new Date().getFullYear() - birthYear;
 
     if (age > 0) {
-        return age + 7;
+        return age + 7 + " in dog years";
     } else {
         return "Less than a year old";
     }
